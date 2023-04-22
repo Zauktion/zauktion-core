af4f3bbd915beb674a607cc6cc79aeb7abe7992b
Juno Chiu <junochiu@Junos-MacBook-Air.local>
2023-04-22 15:36:49 -0400

    add proof generating server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+const port = 8080
+
+app.post('/gen-proof', (req, res)=>{
+    
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
