dd8da221033425295c63a63b67316ddbc5a3d806
Juno Chiu <junochiu@Junos-MacBook-Air.local>
2023-04-22 16:41:54 -0400

    genproof endpoint

---
+++ server/genProof.js
@@ -0,0 +1,93 @@
+import { groth16 } from "snarkjs";
+import * as ff from "ffjavascript";
+import { proofToSCFormat } from "./utils.js";
+
+//import abi from "../configs/zauktion.json";
+const zauktionWasmFile = "./configs/zauktion.wasm";
+const zauktionZkeyFile = "./configs/zauktion.zkey";
+const idcheckWasmFile = "./configs/idcheck.wasm";
+const idcheckZkeyFile = "./configs/idcheck.zkey";
+
+/*
+const rpcURL = `https://rpc.chiadochain.net`;
+const provider = new ethers.providers.JsonRpcProvider(rpcURL, 10200);
+const signer = new ethers.Wallet(process.env.PROJECT_PK_TEST, provider);
+const erc721 = new ethers.Contract(process.env.CONTRACT_TESTNET, abi, signer)
+*/
+
+const zauktionBidProof = async (data) => {
+  try {
+    const bid = BigInt(data.bid);
+    const idSecret = data.idSecret;
+    let idSecretHash = "";
+    for (let i = 0; i < idSecret.length; i++) {
+      idSecretHash += idSecret.charCodeAt(i).toString(16);
+    }
+    // const giveawayTokens = await erc721.mintGiveawayTokens(payload.address, 1);
+    const auctionId = BigInt(10);
+    const x = BigInt(1);
+    const circuitInputs = ff.utils.stringifyBigInts({
+      // Converts the buffer to a BigInt
+      bid: bid,
+      idSecret: idSecretHash,
+      auctionId: auctionId,
+      x: x,
+    });
+    const proof = await groth16.fullProve(
+      circuitInputs,
+      zauktionWasmFile,
+      zauktionZkeyFile
+    );
+    const proofForTx = proofToSCFormat(proof.proof, proof.publicSignals);
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+      },
+      body: JSON.stringify({
+        proofForTxA: ff.utils.stringifyBigInts(proofForTx.a),
+        proofForTxB: ff.utils.stringifyBigInts(proofForTx.b),
+        proofForTxC: ff.utils.stringifyBigInts(proofForTx.c),
+        proofForTxD: ff.utils.stringifyBigInts(proofForTx.pub),
+      }),
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: err.statusCode || 501,
+      body: JSON.stringify(
+        {
+          message: "Error Occured when generating proof!",
+          details: err,
+        },
+        null,
+        2
+      ),
+    };
+  }
+};
+
+const idcheckProof = async (
+  data
+) => {
+  try {
+  } catch (err) {
+    console.error(err);
+    return {
+      statusCode: err.statusCode || 501,
+      body: JSON.stringify(
+        {
+          message: "Error Occured when airdropping!",
+          details: err,
+        },
+        null,
+        2
+      ),
+    };
+  }
+};
+
+
+export {zauktionBidProof, idcheckProof}
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 8080
+import express from "express";
+import { zauktionBidProof } from "./genProof.js";
+const app = express();
+const port = 8080;
 
-app.post('/gen-proof', (req, res)=>{
-    
-})
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+app.post("/gen-proof", async (req, res) => {
+  if (!req.body) {
+   return res.sendStatus(400);
+  }
+  const proof = await zauktionBidProof(req.body);
+  res.status(200).json(proof);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
