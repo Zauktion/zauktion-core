ae74acd0d7782a7761a16d14335bbd083c319638
ryanycw <ryan0213.tem04@nctu.edu.tw>
2023-04-23 10:10:09 +0800

    chore: clean up circuits dir

--- contracts/contracts/EventsFactory.sol
+++ contracts/contracts/EventsFactory.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.4;
 
 // imported contracts and libraries
-import "./Ticket.sol";
 import "./ZKoupon.sol";
 import "./Zauktion.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
@@ -16,36 +15,6 @@ import "./interfaces/IZKoupon.sol";
 import {EventsFactoryStorage} from "./storage/EventsFactory.sol";
 
 contract EventsFactory is Ownable, IEventsFactory, EventsFactoryStorage {
-    function setVerifier(address _verifier) external onlyOwner {
-        verifier = _verifier;
-    }
-
-    function createEvent(
-        string memory _name,
-        string memory _description,
-        uint256 _maxTickets,
-        uint256 _maxCoupons
-    ) external override {
-        if (events[_name].ticket != address(0)) revert("Event already exists");
-        // create Ticket contract
-        Ticket ticket = new Ticket(_maxTickets);
-        // create ZKoupon contract, pass Ticket address in constructor
-        ZKoupon coupon = new ZKoupon(verifier, address(ticket), _maxCoupons);
-        // transferOwnership() to event owner
-        coupon.transferOwnership(msg.sender);
-        ticket.transferOwnership(msg.sender);
-        // write Tickets.sol address to ZKoupon
-        ticket.setCoupon(address(coupon));
-        events[_name] = EventInfo(
-            _description,
-            _maxTickets,
-            _maxCoupons,
-            address(ticket),
-            address(coupon)
-        );
-        emit CreateEvent(address(ticket), address(coupon));
-    }
-
     function createAuction() external override {
         // create Auction contract
         Zauktion auction = new Zauktion();
